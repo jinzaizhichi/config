f37339cf65192962dbd36cb9e880d0aa91c5e15c
HeWenJin <hewenjin@fulu.com>
2021-07-09 11:31:53 +0800

    set jdk env and tree settings

--- nvim/lua/lsp/java/jdtls_setup.lua
+++ nvim/lua/lsp/java/jdtls_setup.lua
@@ -167,6 +167,10 @@ function M.setup()
                 name = "JavaSE-11",
                 path = "/usr/lib/jvm/java-11-openjdk/",
               },
+              {
+                name = "JavaSE-1.8",
+                path = "/usr/lib/jvm/java-8-openjdk/",
+              },
               --[[ {
                 name = "JavaSE-14",
                 path = home .. "/usr/lib/jvm/java-11-openjdk/",
