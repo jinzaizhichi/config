707e4fbf1c120fd2d8e3498736263b2217641419
hewenjin <hewenjin94@outlook.com>
2021-12-01 22:02:33 +0800

    edit toggleterm config

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -4,13 +4,13 @@ return function()
     size = 15,
     -- open_mapping = [[<leader><leader>m]],
     hide_numbers = false, -- hide the number column in toggleterm buffers
-    shade_terminals = true,
+    shade_terminals = false,
     -- shading_factor = '<1>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
     start_in_insert = true,
     insert_mappings = false, -- whether or not the open mapping applies in insert mode
     persist_size = true,
-    direction = 'horizontal',
+    direction = 'float',
     close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell -- change the default shell
+    shell = vim.o.shell, -- change the default shell
   })
 end
