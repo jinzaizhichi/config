915387d826d7ddc5f470af62d16cb2901f417550
HeWenJin <hewenjin94@outlook.com>
2021-12-03 20:17:46 +0800

    update lsp_signature config & keymap

--- nvim/lua/plugins/lsp_signature.lua
+++ nvim/lua/plugins/lsp_signature.lua
@@ -3,6 +3,9 @@ return function ()
     handler_opts = {
       border = 'none'   -- double, single, shadow, none
     },
+    always_trigger = false,
+    floating_window = false,
+    hint_enable = true,
     hint_prefix = " ",
     toggle_key = '<M-s>'
   })
