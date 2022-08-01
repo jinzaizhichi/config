8eeddae55b1af24062bd2d4c312c5e59c5fb5469
hewenjin <hewenjin94@outlook.com>
2022-08-01 14:25:10 +0800

    set lsp log level to debug

--- nvim/lua/plugin/lspconfig.lua
+++ nvim/lua/plugin/lspconfig.lua
@@ -21,7 +21,7 @@ return function()
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
     function(server_name) -- default handler (optional)
-      vim.lsp.set_log_level('trace')
+      vim.lsp.set_log_level('debug')
 
       if server_name ~= "jdtls" then
         local capabilities = vim.lsp.protocol.make_client_capabilities()
