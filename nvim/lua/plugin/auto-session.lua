03e493ef7aaffc65e791087dd0eb8df66359743d
hewenjin <hewenjin94@outlook.com>
2022-12-01 22:44:48 +0800

    edit project.nvim auto-session config

--- nvim/lua/plugin/auto-session.lua
+++ nvim/lua/plugin/auto-session.lua
@@ -2,7 +2,7 @@ return function()
   require('auto-session').setup({
     auto_session_suppress_dirs = { '~/', '/', '~/Desktop/', '~/Music/', '~/Public/', '~/Videos/', '~/Pictures/',
       '~/project/', '~/Documents/', '~/Downloads/', '~/Templates/' },
-    auto_restore_enabled = false,
+    auto_restore_enabled = true,
     auto_session_use_git_branch = true
   })
 end
--- nvim/lua/plugin/project.lua
+++ nvim/lua/plugin/project.lua
@@ -1,7 +1,7 @@
 return function ()
   require("project_nvim").setup {
     -- All the patterns used to detect root dir, when **"pattern"** is in
-    detection_methods = { "lsp", "pattern" },
+    detection_methods = { "pattern", "lsp"  },
     -- detection_methods
     patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
     -- Show hidden files in telescope
