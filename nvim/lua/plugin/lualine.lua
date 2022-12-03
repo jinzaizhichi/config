27e3da468441f80d2fc90775c45cda4a6bb6840b
hewenjin <hewenjin94@outlook.com>
2022-12-03 21:07:45 +0800

    remove separator symbol

--- nvim/lua/plugin/lualine.lua
+++ nvim/lua/plugin/lualine.lua
@@ -18,8 +18,10 @@ return function()
       icons_enabled = true,
       theme = 'auto',
       -- component_separators = { left = '', right = '' },
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
+      -- component_separators = { left = '', right = '' },
+      -- section_separators = { left = '', right = '' },
+      component_separators = { left = '│', right = '│' },
+      section_separators = { left = '', right = '' },
       disabled_filetypes = {
         statusline = {},
         winbar = {},
