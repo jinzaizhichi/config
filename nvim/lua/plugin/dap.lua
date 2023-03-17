eaeab1bf4c9f98608433da4c4a332d3e0bdfbd33
hewenjin <hewenjin94@outlook.com>
2023-03-17 22:00:42 +0800

    edit dap external_terminal

--- nvim/lua/plugin/dap.lua
+++ nvim/lua/plugin/dap.lua
@@ -2,7 +2,7 @@ return function()
   local dap = require('dap')
   dap.defaults.fallback.terminal_win_cmd = 'tabnew'
   dap.defaults.fallback.external_terminal = {
-    command = '/usr/bin/wezterm';
-    args = {'cli', 'spawn'}
+    command = '/usr/bin/tmux';
+    args = {'new-window', '-d'}
   }
 end
