48a87d701ec9e94a99f1897114d903c05b7aaca3
hewenjin <hewenjin94@outlook.com>
2023-03-19 21:42:01 +0800

    update lua ls config

--- nvim/lua/lsp/lua_ls/settings.lua
+++ nvim/lua/lsp/lua_ls/settings.lua
@@ -5,11 +5,17 @@ return {
       version = 'LuaJIT',
       path = vim.split(package.path, ';'),
     },
+    format = {
+      defaultConfig = {
+        align_array_table = false
+      }
+    },
     diagnostics = {
       -- Get the language server to recognize the `vim` global
-      globals = {'vim'},
+      globals = { 'vim' },
     },
     workspace = {
+      checkThirdParty = false,
       -- Make the server aware of Neovim runtime files
       library = vim.api.nvim_get_runtime_file("", true)
       ,
