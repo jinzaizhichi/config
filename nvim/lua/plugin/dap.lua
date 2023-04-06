9e473bd7d80d02b707d2e2d74c576aa593977e07
hewenjin <hewenjin94@outlook.com>
2023-04-06 10:03:31 +0800

    adjust dap terminal win cmd

--- nvim/lua/plugin/dap.lua
+++ nvim/lua/plugin/dap.lua
@@ -3,7 +3,7 @@ return {
   'mfussenegger/nvim-dap',
   config = function()
     local dap = require('dap')
-    dap.defaults.fallback.terminal_win_cmd = 'tabnew'
+    dap.defaults.fallback.terminal_win_cmd = 'enew'
     dap.defaults.fallback.external_terminal = {
       command = '/usr/bin/tmux',
       args = { 'new-window', '-d' }
