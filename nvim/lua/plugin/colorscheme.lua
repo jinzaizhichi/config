b3397ca3d0a2e659e9d94b962d62cdca0c968141
hewenjin <hewenjin94@outlook.com>
2023-07-05 22:42:35 +0800

    integrate highlight with colorscheme

--- nvim/lua/plugin/colorscheme.lua
+++ nvim/lua/plugin/colorscheme.lua
@@ -1,8 +1,26 @@
 return {
-  {'Mofiqul/vscode.nvim', cond = not vim.g.vscode, config = function ()
-    require('vscode').setup({
-      transparent = true
-    })
-  end},
-  {'NvChad/nvim-colorizer.lua', cond = not vim.g.vscode, config = true},
+  {
+    'Mofiqul/vscode.nvim',
+    cond = not vim.g.vscode,
+    config = function()
+      require('vscode').setup({
+        transparent = true,
+        group_overrides = {
+          -- this supports the same val table as vim.api.nvim_set_hl
+          -- use colors from this colorscheme by requiring vscode.colors!
+          TelescopePromptNormal = { link = 'Pmenu' },
+          TelescopeResultsNormal = { link = 'Pmenu' },
+          TelescopeResultsBorder = { link = 'Pmenu' },
+          TelescopePromptBorder = { link = 'Pmenu' },
+          TelescopePromptTitle = { link = 'lualine_a_insert' },
+          TelescopeResultsTitle = { link = 'lualine_a_normal' },
+          TelescopePreviewTitle = { link = 'lualine_a_normal' },
+          ['@interface'] = { link = '@constant' },
+          ['@lsp.type.interface'] = { link = '@interface' },
+          CmpItemMenu = { link = 'Comment' },
+        }
+      })
+    end
+  },
+  { 'NvChad/nvim-colorizer.lua', cond = not vim.g.vscode, config = true },
 }
